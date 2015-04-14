887c7a43c73d5f3bbac58bcecd89630b43fad674
Jon Reed <mr.is@outlook.com>
2015-04-14 11:50:47 -0400

    Added createString test -broke

--- TDDDemo2.Test/Class1.cs
+++ TDDDemo2.Test/Class1.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit;
 
 namespace TDDDemo2.Test
 {
     public class Class1
     {
+
+        [Fact]
+        public void CreateString()
+        {
+            var newString = new textMessage();
+
+            Assert.NotNull(newString);
+        }
     }
 }
