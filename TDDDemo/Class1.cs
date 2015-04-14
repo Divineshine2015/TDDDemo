aac68839e9cbc0bb883e23e22f76cfb98b224c26
Jon Reed <mr.is@outlook.com>
2015-04-14 11:20:31 -0400

    started TDDDemo with one project
    need to add 2nd project

---
+++ TDDDemo/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDDemo
+{
+    public class Class1
+    {
+    }
+}
